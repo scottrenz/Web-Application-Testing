edadb4c6e17beaef23b0b6b662c4eb5c2177d4c3
Scott Renz <scottrenz@scottrenz.com>
2019-08-08 20:03:02 -0400

    made dashboard test look for At Bat Has Ended

--- src/Dashboard.test.js
+++ src/Dashboard.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './Dashboard';
-
+import * as rtl from '@testing-library/react';
+// import 'jest-dom/extend-expect';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Dashboard />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders "At Bat Has Ended', () => {
+    const wrapper = rtl.render(<Dashboard />);
+    expect(wrapper.getByLabelText(/At Bat Has Ended/i));
+  });
