f9f6d7361d9cbcf9894f19299222f026d019f531
Scott Renz <scottrenz@scottrenz.com>
2019-08-09 00:05:58 -0400

    added explanatory text for when at bat can end

--- src/Form.js
+++ src/Form.js
@@ -5,6 +5,7 @@ const FormMy = (props) => {
     
     return (
       <div >
+<p>The at bat ends when the batter makes a hit, is a hit batter, or the batter makes an out.</p>
         <Dashboard users={props.users} setUsers={props.setUsers} />
     </div>
   );
